4ca3c7010cb64bb481aaedf838d23056b9573aa2
masuyama13 <masterxwebm13@gmail.com>
2025-04-16 11:25:45 -0700

    Add a line

--- GitDemoWeek15/src/test/Test.java
+++ GitDemoWeek15/src/test/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Demo");
 		System.out.println("Add this by a member");
+		System.out.println("Another branch");
 	}
 
 }
